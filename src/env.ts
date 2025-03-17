70b9b0d9eae01923c8c70a0a3877eb33b8a21aeb
Woofer <61364477+Woofer21@users.noreply.github.com>
2025-03-17 08:35:26 -0400

    ci: builds now work with envs present (#11)
    * ci: see if this allows build workflow enviroments
    
    * ci: allows build workflow enviroments take 2
    
    * ci: allows build workflow enviroments take 3
    
    * ci: allows build workflow enviroments take 4
    
    * ci: allows build workflow enviroments take 5

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,9 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-	server: {},
+	server: {
+		UPSTASH_API_KEY: z.string(),
+	},
 	client: {
 		// client variables are public and can be accessed by the client
 	},
