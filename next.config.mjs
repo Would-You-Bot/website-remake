0cb3724b87d17fa19405d28bfd41cf64f34fda01
Woofer21 <61364477+Woofer21@users.noreply.github.com>
2025-03-16 21:38:17 -0400

    feat: nextjs redirects

--- next.config.mjs
+++ next.config.mjs
@@ -55,6 +55,46 @@ const nextConfig = {
 			},
 		];
 	},
+	async redirects() {
+		return [
+			{
+				source: "/support",
+				destination: "https://discord.com/invite/vMyXAxEznS",
+				permanent: true,
+			},
+			{
+				source: "/rivo",
+				destination: "https://rivo.gg",
+				permanent: true,
+			},
+			{
+				source: "/invite",
+				destination:
+					"https://discord.com/oauth2/authorize?client_id=981649513427111957&permissions=275415247936&scope=bot%20applications.commands",
+				permanent: true,
+			},
+			{
+				source: "/reddit",
+				destination: "https://www.reddit.com/r/WouldYou/",
+				permanent: true,
+			},
+			{
+				source: "/discord",
+				destination: "/support",
+				permanent: true,
+			},
+			{
+				source: "/imprint",
+				destination: "/legal",
+				permanent: true,
+			},
+			{
+				source: "/impressum",
+				destination: "/legal-de",
+				permanent: true,
+			},
+		];
+	},
 };
 
 export default nextConfig;
