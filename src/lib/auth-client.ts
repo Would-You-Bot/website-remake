90b05834b6456eca8f1dc8e1abeb5310e768580e
Taqib <taqib.ibrahim@gmail.com>
2025-03-17 10:26:26 +0100

    fix: use env

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
+import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-	baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:2123",
+	baseURL: env.NEXT_PUBLIC_APP_URL || "http://localhost:2123",
 });
