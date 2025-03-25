70e9ca6e95ea0e1f21870af36e570df1ea9f2fa3
dpaulos6 <itzframepvp@outlook.com>
2025-03-25 09:46:25 +0000

    refactor!: correct jest setup for app router

--- src/app/(marketing)/page.test.tsx
+++ src/app/(marketing)/page.test.tsx
@@ -2,11 +2,20 @@
  * @jest-environment jsdom
  */
 import Page from "@/app/(marketing)/page";
+import translations from "@/i18n/translations/en.json";
 import { render, screen } from "@testing-library/react";
+import { NextIntlClientProvider } from "next-intl";
 
 describe("Home", () => {
 	it("renders a heading", () => {
-		render(<Page />);
+		render(
+			<NextIntlClientProvider
+				locale="en"
+				messages={translations}
+			>
+				<Page />
+			</NextIntlClientProvider>
+		);
 
 		const heading = screen.getByRole("heading", { level: 1 });
 
