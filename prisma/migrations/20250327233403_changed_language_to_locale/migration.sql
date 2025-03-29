6f6654868149efce9c4d339624ab7f3d8eaad715
Woofer21 <61364477+Woofer21@users.noreply.github.com>
2025-03-29 11:00:56 -0400

    chore: clean up migrations

--- prisma/migrations/20250327233403_changed_language_to_locale/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `language` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "language",
-ADD COLUMN     "locale" TEXT NOT NULL DEFAULT 'en';
--- prisma/migrations/20250327234009_revert/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `locale` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "locale",
-ADD COLUMN     "language" TEXT NOT NULL DEFAULT 'en';
